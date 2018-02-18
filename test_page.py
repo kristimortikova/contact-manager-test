b7e03bafc0e47d256184878d423673604972569c
kristimortikova <kristimortikova@gmail.com>
2018-02-18 16:22:54 +0200

    created separate pages

--- test_page.py
+++ test_page.py
@@ -5,13 +5,7 @@ def test_example(selenium):
  	selenium.get('http://localhost:3000')
  	assert selenium.title == 'Portal link'
  	assert selenium.current_url == 'http://localhost:3000/'
- 	counter = selenium.find_element_by_css_selector(".App-intro .counter")
- 	assert counter.text == '0'
- 	button = selenium.find_element_by_css_selector(".App-intro")
- 	for x in range(10): 
- 		assert counter.text == str(x)
- 		button.click()
- 	assert	counter.text == '10'
+ 	assert not selenium.find_elements_by_css_selector(".App-intro .counter")
 
 
 def test_example_page(selenium):
